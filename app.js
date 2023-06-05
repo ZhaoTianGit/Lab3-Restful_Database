bca2a9a653f9f8ea58dbdb98d8c87697bdd830bf
Zhao Tian <zhaotian.org@gmail.com>
2023-06-05 11:26:36 +0800

    Patch Update function completed

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ async function run() {
     // await client.db("admin").command({ ping: 1 });
     // console.log("Pinged your deployment. You successfully connected to MongoDB!");
 }
-// run().catch(console.dir);
+
 run()
     .then(console.log)
     .catch(console.error)
@@ -61,44 +61,73 @@ app.get('/usersGet', async (req, res) => {
 
         const users = await collection.find().toArray();
         res.status(200).json(users);
-        client.close();
+        ///client.close(); // Do not close the client to get the info again
     } catch (err) {
         res.status(500).send(err);
     }
 });
 
 // PATCH request - Updata user's data
+// app.patch('/usersUpdate', async (req, res) => {
+//   try {
+//     const db = client.db(dbName);
+//     const collection = db.collection(dbCollection);
+//     console.log("Update Starto!!!!!");
+//     const filter = { matrixNo: req.body.matrixNo }; // Assuming you have a "matrixNo" field to uniquely identify the document
+//     console.log(filter);
+//     const update = { $set: req.body.matrixNo }; // Assuming the entire req.body contains the updated fields/values
+//     console.log(update);
+
+//     const result = await collection.updateOne(filter, update);
+//     res.send(result);
+//     console.log("User with", req.body.matrixNo, "is updated successfully!");
+//   } catch (err) {
+//     res.status(500).send(err);
+//   }
+// });
+
+// PATCH Update v2
 app.patch('/usersUpdate', async (req, res) => {
   try {
     const db = client.db(dbName);
     const collection = db.collection(dbCollection);
-    console.log("Update Starto!!!!!");
-    const filter = { matrixNo: req.body.matrixNo }; // Assuming you have a "matrixNo" field to uniquely identify the document
+
+    const filter = { matrixNo: req.body.matrixNo };
+    const update = {
+      $set: {
+        name: req.body.name,
+        password: req.body.password,
+        matrixNo: req.body.matrixNo
+      }
+    };
     console.log(filter);
-    const update = { $set: req.body.matrixNo }; // Assuming the entire req.body contains the updated fields/values
     console.log(update);
 
     const result = await collection.updateOne(filter, update);
-    res.send(result);
-    console.log("User with", req.body.matrixNo, "is updated successfully!");
+    console.log(result);
+    if (result.matchedCount === 1) {
+      res.send('User updated successfully!');
+      console.log("User with", req.body.matrixNo, "is updated successfully!");
+    } else {
+      res.status(404).send('User not found.');
+    }
   } catch (err) {
-    res.status(500).send(err);
+    console.error(err);
+    res.status(500).send('An error occurred while updating the user.');
+  }
+});
+
+app.delete('/usersDelete', async (req, res) => {
+  try {
+      const db = client.db(dbName);
+      const collection = db.collection(dbCollection);
+
+      
+      
+  } catch (err) {
+      res.status(500).send(err);
   }
 });
 
 
-app.post('/insertData', async(req, res)=>{
-  try{
-    const sensorEvent = req.body;
-    const result = await client.db(dbName).collection(dbCollection).insertMany(sensorEvent);
-    if(result){
-      req.send(sensorEvent);
-      console.log(sensorEvent);
-    }
 
-  }catch(err){
-  res.status(500).send(err);
-  res.send({error:"User not found"});
-  console.log("Error found during inserting data");
-}
-});
